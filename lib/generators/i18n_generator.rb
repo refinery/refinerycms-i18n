8e3a11a65e7bdaa4c4f3d49ec7c7e2e8a42fff42
Philip Arndt <parndt@gmail.com>
2011-07-14 14:59:59 +1200

    Renamed to refinery:i18n generator

---
+++ lib/generators/i18n_generator.rb
@@ -0,0 +1,14 @@
+require 'refinery/generators'
+
+module ::Refinery
+  class I18nGenerator < ::Refinery::Generators::EngineInstaller
+
+    source_root File.expand_path('../../../', __FILE__)
+    engine_name "i18n"
+
+    def generate
+      copy_file 'config/i18n-js.yml', Rails.root.join('config', 'i18n-js.yml')
+    end
+
+  end
+end
