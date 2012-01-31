e9ef7026f1399bcfa509eef37628c5445417a606
Josef Šimánek <retro@ballgag.cz>
2012-01-31 10:43:56 +0100

    initializer refinery_i18n.rb.erb -> refinery/i18n.rb

--- lib/generators/refinery/i18n_generator.rb
+++ lib/generators/refinery/i18n_generator.rb
@@ -3,7 +3,7 @@ module Refinery
     source_root File.expand_path('../templates', __FILE__)
 
     def generate_refinery_i18n_initializer
-      template "config/initializers/refinery_i18n.rb.erb", File.join(destination_root, "config", "initializers", "refinery_i18n.rb")
+      template "config/initializers/refinery/i18n.rb.erb", File.join(destination_root, "config", "initializers", "refinery", "i18n.rb")
     end
 
   end
