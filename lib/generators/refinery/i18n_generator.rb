59ef794b12fd87303f4cdb7aa7806a3705164f8b
Philip Arndt <parndt@gmail.com>
2013-01-18 13:24:28 +1300

    Pull in any migrations when the generator runs.

--- lib/generators/refinery/i18n_generator.rb
+++ lib/generators/refinery/i18n_generator.rb
@@ -2,8 +2,13 @@ module Refinery
   class I18nGenerator < Rails::Generators::Base
     source_root File.expand_path('../templates', __FILE__)
 
+    def rake_db
+      rake "refinery_i18n:install:migrations"
+    end
+
     def generate_refinery_i18n_initializer
-      template "config/initializers/refinery/i18n.rb.erb", File.join(destination_root, "config", "initializers", "refinery", "i18n.rb")
+      template "config/initializers/refinery/i18n.rb.erb",
+               File.join(destination_root, "config", "initializers", "refinery", "i18n.rb")
     end
 
   end
