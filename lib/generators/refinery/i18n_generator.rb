b17f6f284279e2cb795ef4430d71d1dc6792f063
Philip Arndt <parndt@gmail.com>
2013-01-22 08:55:36 +1300

    Revert "Pull in any migrations when the generator runs."
    We have no migrations so this just errors rather than just silently failing.
    
    This reverts commit 59ef794b12fd87303f4cdb7aa7806a3705164f8b.

--- lib/generators/refinery/i18n_generator.rb
+++ lib/generators/refinery/i18n_generator.rb
@@ -2,10 +2,6 @@ module Refinery
   class I18nGenerator < Rails::Generators::Base
     source_root File.expand_path('../templates', __FILE__)
 
-    def rake_db
-      rake "refinery_i18n:install:migrations"
-    end
-
     def generate_refinery_i18n_initializer
       template "config/initializers/refinery/i18n.rb.erb",
                File.join(destination_root, "config", "initializers", "refinery", "i18n.rb")
