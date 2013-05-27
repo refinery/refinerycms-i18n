d2b13e1497d3eec4cbd53ab0cc8d47f73baf1add
benxao <benxaooi@gmail.com>
2013-05-27 18:14:13 +0700

    add domain name based i18n

--- lib/refinery/i18n-filter.rb
+++ lib/refinery/i18n-filter.rb
@@ -2,8 +2,11 @@ module RoutingFilter
   class RefineryLocales < Filter
 
     def around_recognize(path, env, &block)
-      if ::Refinery::I18n.url_filter_enabled?
-        if path =~ %r{^/(#{::Refinery::I18n.locales.keys.join('|')})(/|$)}
+      if ::Refinery::I18n.url_filter_enabled? || ::Refinery::I18n.domain_name_enabled?
+        if ::Refinery::I18n.domain_name_enabled?
+          hostname = env['HTTP_HOST'].sub(/:\d+$/, '')
+          ::I18n.locale = ::Refinery::I18n.domains_locales[hostname] || ::Refinery::I18n.default_frontend_locale
+        elsif path =~ %r{^/(#{::Refinery::I18n.locales.keys.join('|')})(/|$)}
           path.sub! %r(^/(([a-zA-Z\-_])*)(?=/|$)) do
             ::I18n.locale = $1
             ''
@@ -24,7 +27,7 @@ module RoutingFilter
 
       yield.tap do |result|
         result = result.is_a?(Array) ? result.first : result
-        if ::Refinery::I18n.url_filter_enabled? and
+        if ::Refinery::I18n.url_filter_enabled? and !::Refinery::I18n.domain_name_enabled? and
            locale != ::Refinery::I18n.default_frontend_locale and
            result !~ %r{^/(#{Refinery::Core.backend_route}|wymiframe)}
           result.sub!(%r(^(http.?://[^/]*)?(.*))) { "#{$1}/#{locale}#{$2}" }
