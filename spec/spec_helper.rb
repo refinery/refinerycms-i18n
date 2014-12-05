31068b0b5371e7e8d4a1a9b268edc3e1ea16e7b7
Philip Arndt <git@arndt.io>
2014-12-05 11:12:06 -0800

    The tests are not using JS so we don't need to set Capybara.javascript_driver

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -17,9 +17,6 @@ RSpec.configure do |config|
   config.run_all_when_everything_filtered = true
 end
 
-# set javascript driver for capybara
-Capybara.javascript_driver = :selenium
-
 # Requires supporting files with custom matchers and macros, etc,
 # in ./support/ and its subdirectories including factories.
 ([Rails.root.to_s] | ::Refinery::Plugins.registered.pathnames).map{|p|
